0d0da4a1a11c397fda8da064fc4db73f60d52b0e
sabrina hernandez <sabrinaguzman2009@gmail.com>
2023-08-03 18:06:21 -0400

    add img footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,29 +2,39 @@ import { faGithub, faLinkedin, faTwitter } from "@fortawesome/free-brands-svg-ic
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Col, Container, Row } from "react-bootstrap";
 import logo from '../assets/img/logo-no-background.png';
+import FooterImg from '../assets/img/wallpaperflare.com_wallpaper.jpg';
 
-
-
-const Footer = () => {
-    return (
-        <footer className="footer">
-            <Container>
-                <Row className="align-item-center">
-                    <Col sm={6}>
-                    <img className='logo' src={logo} alt='logo'/>
-                        <p>© 2023 - All Rights Reserved</p>
-                    </Col>
-                    <Col sm={6}>
-                        <FontAwesomeIcon icon={faGithub}  />
-
-                        <FontAwesomeIcon icon={faLinkedin} />
-
-                        <FontAwesomeIcon icon={faTwitter}  />                   
-                                        </Col>
-                </Row>
-            </Container>
-        </footer>
-    );
+const Footer: React.FC = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          <Col sm={6} className="text-left">
+            <img className="logo" src={logo} alt="logo" />
+            <p>© 2023 - All Rights Reserved</p>
+          </Col>
+          <Col sm={6} className="text-right">
+            <div className="social-icons">
+              <a href="#" className="social-icon">
+                <FontAwesomeIcon icon={faGithub} />
+              </a>
+              <a href="#" className="social-icon">
+                <FontAwesomeIcon icon={faLinkedin} />
+              </a>
+              <a href="#" className="social-icon">
+                <FontAwesomeIcon icon={faTwitter} />
+              </a>
+            </div>
+          </Col>
+        </Row>
+        <Row className="align-items-center justify-content-end">
+          <Col xs={12} md={6} xl={5}>
+            <img src={FooterImg} alt="FooterImg" />
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 };
 
 export default Footer;
