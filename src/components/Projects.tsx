5663cee322a06571c7ad00902850253d27769190
sabrina hernandez <sabrinaguzman2009@gmail.com>
2023-08-03 16:52:38 -0400

    add img proyects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,37 +1,40 @@
 import React from "react";
 import { Col, Container, Nav, Row, Tab} from "react-bootstrap";
 import ProjectsCard from "./ProjectsCard";
-import image from '../assets/img/project01.png';
+import image1 from '../assets/img/project01.png';
+import image2 from '../assets/img/crudstudent.png';
+import image3 from '../assets/img/crudemployee.png';
+import image4 from '../assets/img/lolitaweb.png';
 
 const Projects: React.FC = () => {
   const projects = [
     {
       title: 'PokeDex App',
       description: 'This is a project that I created using React and TypeScript. It is a PokeDex app that allows you to search for Pokemon and view their stats. It also allows you to filter Pokemon by type. I used the PokeAPI to get the data for the Pokemon. I used React Bootstrap for the styling. I used TypeScript for the type checking.',
-      image: `${image}`,
+      image: `${image1}`,
       link: 'https://pokedex-react-rho-five.vercel.app/?vercelToolbarCode=342lDgqzYengYGB',
       github: 'https://github.com/pancakecodes/pokedex-react'
     },
     {
-      title: 'Project 2',
-      description: 'This is a project',
-      image: '',
-      link: '',
-      github: ''
+      title: 'Gestion Estudiantes',
+      description: 'This is a project that I created using .net core and React.',
+      image: `${image2}`,
+      link: 'https://github.com/pancakecodes/crudEstudiante',
+      github: 'https://github.com/pancakecodes/crudEstudiante'
     },
     {
-      title: 'Project 3',
-      description: 'This is a project',
-      image: '',
-      link: '',
-      github: ''
+      title: 'Gestion Empleado',
+      description: 'This is a project that I created using Angular',
+      image: `${image3}`,
+      link: 'https://github.com/pancakecodes/crud-employee',
+      github: 'https://github.com/pancakecodes/crud-employee'
     },
     {
-      title: 'Project 4',
-      description: 'This is a project',
-      image: '',
-      link: '',
-      github: ''
+      title: 'Lolita Website',
+      description: 'This is a project that I created in base of a design that I found in the internet.',
+      image: `${image4}`,
+      link: 'https://github.com/pancakecodes/lolitawebsite',
+      github: 'https://github.com/pancakecodes/lolitawebsite'
     }
   ];
 
